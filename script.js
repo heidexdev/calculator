0823f4d14ca45b2dd9a43245d23cfbc565276b36
heidexdev <heidexdev@gmail.com>
2026-07-01 21:18:52 +0330

    Add event listeners for calculator buttons and initialize state

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function multiply(num1, num2) {
 function operate(num1, num2, operator) {
   return operator(num1, num2);
 }
+
+let state = {
+  num1: null,
+  num2: null,
+  operator: null,
+  result: null,
+};
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+  });
+});
